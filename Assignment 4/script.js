1851f72f463482f213f309a605aecbc07771111c
sandyloki2308 <sandyloki2308@gmail.com>
2022-04-06 13:15:23 +0530

    Update

---
+++ Assignment 4/script.js
@@ -0,0 +1,29 @@
+var names= new Array();
+names[0]="Lokesh";
+names[1]="Jagan";
+names[2]="john";
+names[3]="jagathish";
+names[4]="santhosh";
+names[5]="manoj";
+names[6]="saravanan";
+names[7]="poovarasan";
+names[8]="jolly";
+names[9]="joy";
+
+
+for(var i=0;i<names.length;i++){
+
+  if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j')
+  
+   {
+            console.log("goodbye " +names[i]);
+
+    }
+  else {
+
+           console.log("hello " +names[i]);
+       }
+    
+}	
+
+
